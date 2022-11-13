8801d160df4d57793f620d2382ef3fcf6d04b125
Алексей Шевченко <alex18w@mail.ru>
2022-11-13 18:08:22 +0500

    Работа над ошибками

--- descr_2.py
+++ descr_2.py
@@ -1,28 +1,28 @@
-class NonNegative_Int:
+class NonNegativeInt:
 
     def __set__(self, instance, value):
         if isinstance(value, int):
             instance.__dict__[self.my_attr] = value
         else:
             raise TypeError("Может быть только целым числом")
-        
+
         if value < 0:
             raise ValueError("Не может быть отрицательным")
-        instance.__dict__[self.my_attr] = value
 
     def __set_name__(self, owner, my_attr):
         self.my_attr = my_attr
 
 
 class Worker:
-    wage = NonNegative_Int()
-    bonus = NonNegative_Int()
+    wage = NonNegativeInt()
+    bonus = NonNegativeInt()
 
     def __init__(self, name, surname, position, wage, bonus):
         self.name = name
         self.surname = surname
         self.position = position
         self._income = {"wage": wage, "bonus": bonus}
+
 
 class Position(Worker):
 
@@ -35,12 +35,13 @@ class Position(Worker):
     def get_total_income(self):
         return self._income.get('wage') + self._income.get('bonus')
 
+
 OBJ = Position('Сергей', 'Иванов', 'Инженер', 100000, 25000)
 print(OBJ.get_total_income())
 
-# OBJ.wage = -100000
-# OBJ.bonus = 25000
-# print(OBJ.get_total_income())
+OBJ.wage = -100000
+OBJ.bonus = 25000
+print(OBJ.get_total_income())
 
 OBJ.wage = 100000
 OBJ.bonus = 25000.5
